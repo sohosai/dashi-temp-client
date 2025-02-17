2aeab9075ebdf0ea37e599c1d45b3c5d303583b7
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-02-17 10:31:48 +0900

    wip

--- src/components/RegisterItemForm.tsx
+++ src/components/RegisterItemForm.tsx
@@ -3,6 +3,7 @@ import { Color, registerItemSchema, RegisterItemSchemaType } from '../validation
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FC } from 'react';
 import { ErrorMessage } from '@hookform/error-message';
+import { RegisterItemRequest } from '../model/registerItemRequest';
 
 const RegisterItemForm: FC = () => {
   const {
@@ -14,7 +15,34 @@ const RegisterItemForm: FC = () => {
     resolver: zodResolver(registerItemSchema),
   });
   const onSubmit: SubmitHandler<RegisterItemSchemaType> = (data) => {
-    console.info(data);
+    // conver from zod schema to api schema
+    const requestColor: string = data.color.map((color) => color.color).join('^');
+    const requestPurchaseYear: number | null = Number.isNaN(data.purchase_year) ? null : data.purchase_year;
+    const requestPurchasePrice: number | null = Number.isNaN(data.purchase_price) ? null : data.purchase_price;
+    const requestDurability: number | null = Number.isNaN(data.durability) ? null : data.durability;
+    const requestData: RegisterItemRequest = {
+      name: data.name,
+      visible_id: data.visible_id,
+      parent_visible_id: data.parent_visible_id,
+      product_number: data.product_number,
+      description: data.description,
+      purchase_year: requestPurchaseYear,
+      purchase_price: requestPurchasePrice,
+      durability: requestDurability,
+      is_depreciation: data.is_depreciation,
+      connector: data.connector.map((connector) => connector.connector),
+      color: requestColor,
+    };
+    console.debug('requestData');
+    console.info(requestData);
+    // send
+    fetch(`http://localhost:5000/api/item/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    }).catch((err) => console.error(err));
   };
   const connectorArray = useFieldArray({
     name: 'connector',
